826721c10c165fcef4a0aa3b5106f9fe59ccf941
ryan.zhuang <ryan.zhuang@vipshop.com>
2016-09-29 17:22:55 +0800

    first project

---
+++ my-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <loading :show="isLoading" position="absolute"></loading>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="less">
+@import '~vux/src/styles/reset';
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
+
